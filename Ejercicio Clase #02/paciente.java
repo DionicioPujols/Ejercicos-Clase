bc8b493735d16625bc5faeee66c8e044aa3f2e47
DionicioPujols <dionicio_hernandez@ucne.edu.do>
2026-05-18 20:09:17 -0400

    Agregar clase paciente

---
+++ Ejercicio Clase #02/paciente.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class paciente{
+    
+    private String nombre;
+    private int fechaNacimiento;
+    private String telefono;
+    private String direccion;
+    private String sexo;
+    private String cedula;
+    private boolean seguro;
+
+    public paciente(String nombre, int fechaNacimiento, String  telefono, String direccion, String sexo, String cedula, boolean seguro ){
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.sexo = sexo;
+        this.cedula = cedula;
+        this.seguro = seguro
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(int fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public boolean isSeguro() {
+        return seguro;
+    }
+
+    public void setSeguro(boolean seguro) {
+        this.seguro = seguro;
+    }
+
+}
